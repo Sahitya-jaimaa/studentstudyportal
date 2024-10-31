751217c689b182e6a3a1c7bf09fe1b0c02d5209e
Sahitya-jaimaa <sahityajaimaa@gmail.com>
2024-10-31 20:58:40 +0530

    Added test view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from . import views,test_view
 
 urlpatterns=[
     path('',views.home,name="home"),
+    path('test/', test_view, name='test-view'),
     path('notes',views.notes,name="notes"),
     path('delete_note/<int:pk>',views.delete_note,name="delete-note"),
     path('notes_detail/<int:pk>',views.NotesDetailView.as_view(),name="notes-detail"),
